9eba34c0dd3b7e112868961f78464f330c48b901
Luka-CB <lukaaslamazashvili20@gmail.com>
2023-12-28 09:04:22 -0800

    Changed token

--- src/utils/axios.ts
+++ src/utils/axios.ts
@@ -5,6 +5,6 @@ export default axios.create({
   headers: {
     "Content-Type": "application/json",
     Authorization:
-      "Bearer ce8cdaf18bf0ca4a945dba410b3ceac0128df681563c04446f0915537ee7fcab",
+      "Bearer d9863d8d5efdda1375f379249e9614da88c403d52fcb0812b92522468253dfeb",
   },
 });
